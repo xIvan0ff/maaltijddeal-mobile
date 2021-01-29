89c7bf7616d4b4af9ff20cd1971e9e423ed6c7ce
bobymoby <imbobimobi@gmail,com>
2021-01-29 17:29:19 +0200

    filters

--- components/PriceComponent.tsx
+++ components/PriceComponent.tsx
@@ -1,4 +1,6 @@
+import { useColors } from "@hooks/useColors"
 import { createStyles } from "@styles/createStyles"
+import { elevation } from "@styles/elevation"
 import { price as convertPrice } from "@utils/price"
 import React from "react"
 import { TouchableOpacity } from "react-native"
@@ -15,15 +17,23 @@ export const PriceComponent: React.FC<IPriceComponent> = ({
     onSelect,
     selected = false,
 }) => {
+    const colors = useColors()
     return (
         <View
             style={{
                 ...styles.priceButtonContainer,
-                backgroundColor: selected ? "orange" : "white",
+                ...(selected
+                    ? { backgroundColor: colors.primary }
+                    : { ...elevation(4) }),
             }}
         >
             <TouchableOpacity style={styles.priceButton} onPress={onSelect}>
-                <Text style={styles.priceButtonText}>
+                <Text
+                    style={{
+                        ...styles.priceButtonText,
+                        color: !selected ? colors.primary : "white",
+                    }}
+                >
                     {"<"}
                     {convertPrice(price)}
                 </Text>
@@ -36,9 +46,9 @@ const styles = createStyles({
     priceButtonContainer: {
         width: 80,
         height: 40,
-        elevation: 4,
         borderRadius: 100,
         backgroundColor: "white",
+        marginHorizontal: 5,
     },
     priceButton: {
         height: "100%",
